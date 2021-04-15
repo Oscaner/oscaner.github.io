14e0ede6ddded1347460d841f1901eba8e9a4974
Oscaner Miao <oscaner1997@gmail.com>
2021-04-15 16:53:30 +0800

    Oscaner - GLOBAL - Add support for directory listing page.

---
+++ _plugins/permalink-custom-vars.rb
@@ -0,0 +1,17 @@
+module Jekyll
+
+  module Drops
+    class UrlDrop < Drop
+      def simple_path
+        post_root = Pathname.new('_posts')
+        Pathname.new(@obj.relative_path).relative_path_from(post_root).dirname.to_s
+      end
+
+      def directory_path
+        post_root = Pathname.new('_directories')
+        Pathname.new(@obj.relative_path).relative_path_from(post_root).dirname.to_s
+      end
+    end
+  end
+
+end
